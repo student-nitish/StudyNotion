69d9334eb99083201e505e3da60526c68fe01d64
Nitish Kumar Rajak <nrajak16@gmail.com>
2025-12-26 00:05:18 +0530

    update

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:[ "http://localhost:3000",
+		         "https://study-notion-two-psi.vercel.app/",
+	   ],
 		credentials:true,
 	})
 )
